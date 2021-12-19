b59a6871d433c9c78e05ef5094d8240586c4bf8d
ahowatt <50527662+ahowatt@users.noreply.github.com>
2021-12-19 12:20:29 -0800

    change scene on actionEvent

--- src/controller/MainMenu.java
+++ src/controller/MainMenu.java
@@ -1,7 +1,12 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,6 +19,12 @@ public class MainMenu implements Initializable {
     }
 
     public void toAddPart(ActionEvent actionEvent) throws IOException {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/view/addPart.fxml"));//set up the top hierarchy of the new "page"
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow(); //set it up as a button action event,
+        //and then cast it to a stage
+        Scene scene = new Scene(root,800,600);
+        stage.setTitle("Add Parts");
+        stage.setScene(scene); //pass the created scene to the stage
+        stage.show();
     }
 }
