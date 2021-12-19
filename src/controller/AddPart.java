cff0bb3878641c88cac9f9162342462e684ccd97
ahowatt <50527662+ahowatt@users.noreply.github.com>
2021-12-19 12:27:28 -0800

    return to Main Menu

--- src/controller/AddPart.java
+++ src/controller/AddPart.java
@@ -1,7 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,5 +16,15 @@ public class AddPart implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void toMainMenu(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));//set up the top hierarchy of the new "page"
+        Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow(); //set it up as a button action event,
+        //and then cast it to a stage
+        Scene scene = new Scene(root,800,600);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene); //pass the created scene to the stage
+        stage.show();
     }
 }
